572db8c1fd1847aababc23658ee459444db7db42
nikoms <nikoms@gmail.com>
2014-08-21 22:30:19 +0200

    Add listener and test it with TDD

---
+++ src/DynamicFixtureListener.php
@@ -0,0 +1,93 @@
+<?php
+
+
+namespace Nikoms\DynamicFixture;
+
+
+use Exception;
+use PHPUnit_Framework_AssertionFailedError;
+use PHPUnit_Framework_Test;
+use PHPUnit_Framework_TestSuite;
+
+class DynamicFixtureListener implements \PHPUnit_Framework_TestListener
+{
+
+    public function addError(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+    }
+
+    public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
+    {
+    }
+
+    public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+    }
+
+    public function addRiskyTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+    }
+
+    public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+    }
+
+    public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
+    {
+    }
+
+    public function endTestSuite(PHPUnit_Framework_TestSuite $suite)
+    {
+    }
+
+    public function endTest(PHPUnit_Framework_Test $test, $time)
+    {
+    }
+
+    /**
+     * A test started.
+     * @param  PHPUnit_Framework_Test $test
+     */
+    public function startTest(PHPUnit_Framework_Test $test)
+    {
+        //That's nasty, but saved my life :)
+        if ($test instanceof \PHPUnit_Framework_TestCase) {
+            $this->setUpContext($test);
+        }
+    }
+
+    /**
+     * @param \PHPUnit_Framework_TestCase $testCase
+     */
+    private function setUpContext(\PHPUnit_Framework_TestCase $testCase)
+    {
+        $reflectionMethod = new \ReflectionMethod($testCase, $testCase->getName(false));
+        $this->callMethods($testCase, $this->getAnnotations($reflectionMethod, 'setUpContext'));
+    }
+
+    /**
+     * @param \ReflectionMethod $reflectionMethod
+     * @param $tagName
+     * @return mixed
+     */
+    private function getAnnotations(\ReflectionMethod $reflectionMethod, $tagName)
+    {
+        preg_match_all("/@$tagName (.*)(\\r\\n|\\r|\\n)/U", $reflectionMethod->getDocComment(), $matches);
+        return $matches[1];
+    }
+
+    /**
+     * @param \PHPUnit_Framework_TestCase $testCase
+     * @param array $methods
+     */
+    private function callMethods(\PHPUnit_Framework_TestCase $testCase, array $methods)
+    {
+        foreach ($methods as $method) {
+            if(!method_exists($testCase, $method)){
+                continue;
+            }
+            $reflectionMethod = new \ReflectionMethod($testCase, $method);
+            $reflectionMethod->invoke($testCase);
+        }
+    }
+} 
--- tests/DynamicFixtureListenerTest.php
+++ tests/DynamicFixtureListenerTest.php
@@ -2,9 +2,55 @@
 
 class DynamicFixtureListenerTest extends PHPUnit_Framework_TestCase {
 
-    public function testTrue()
+    private $name;
+    private $firstName;
+
+    public function setUpName()
     {
-        $this->assertTrue(true);
+        $this->name = 'Name';
+    }
+
+    public function setUpFirstName()
+    {
+        $this->firstName = 'FirstName';
+    }
+
+    /**
+     * @setUpContext setUpName
+     */
+    public function testSetUpName()
+    {
+        $this->assertSame('Name', $this->name);
+        $this->assertNull($this->firstName);
+    }
+
+    /**
+     *
+     */
+    public function testNoSetUp()
+    {
+        $this->assertNull($this->name);
+        $this->assertNull($this->firstName);
+    }
+
+    /**
+     *
+     * @setUpContext setUpName
+     * @setUpContext setUpFirstName
+     */
+    public function testSetUpNameAndFirstName()
+    {
+        $this->assertSame('Name', $this->name);
+        $this->assertSame('FirstName', $this->firstName);
+    }
+
+    /**
+     * @setUpContext undefinedMethod
+     */
+    public function testSetUpWithNonExistentMethod()
+    {
+        $this->assertNull($this->name);
+        $this->assertNull($this->firstName);
     }
 }
  
